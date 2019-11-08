2a9da2af947137f413c9a99b26b1479b8e7fd77e
surya <surya00060@gmail.com>
2019-11-08 22:13:38 +0530

    Added mv3 large and v2

---
+++ mv3-large-sys-2/script.py
@@ -0,0 +1,3 @@
+python train.py --lr=3e-4 --optim=adam --epoch=60 --name=v3l
+python train.py --lr=1e-3 --optim=sgd --epoch=30 --name=v3l --resume
+python train.py --lr=1e-4 --optim=sgd --epoch=30 --name=v3l --resume
---
+++ mv3-large-sys/script.py
@@ -0,0 +1,3 @@
+python train.py --lr=3e-4 --optim=adam --epoch=100 --name=v3l
+python train.py --lr=1e-3 --optim=sgd --epoch=30 --name=v3l --resume
+python train.py --lr=1e-5 --optim=sgd --epoch=30 --name=v3l --resume
